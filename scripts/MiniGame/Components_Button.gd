a135ec78f5e3f5e716d893c90edc791f460b268c
BibiFloris <f.pruijt@gmail.com>
2023-01-18 17:12:16 +0100

    Puzzle button changes state

--- scripts/MiniGame/Components_Button.gd
+++ scripts/MiniGame/Components_Button.gd
@@ -3,18 +3,25 @@ extends TextureRect
 var close_button_texture = "res://assets/Textures/Overlay_components/close_button.png"
 var complete_button_off_texture = "res://assets/Textures/Overlay_components/complete_button_off.png"
 var complete_button_on_texture = "res://assets/Textures/Overlay_components/complete_button_on.png"
-var is_level_complete
+var is_level_complete = false
 
 
-func _on_toggle_simulation(is_simulating: bool, level_comple:bool):
-	if is_simulating:
-		texture = load(complete_button_off_texture)
-	else:
-		texture = load(close_button_texture)
-	is_level_complete = level_comple
+func _on_toggle_simulation(is_simulating: bool):
+	pass
+#	if is_level_complete:
+#		texture = load(complete_button_on_texture)	
+#	elif is_simulating:
+#		texture = load(complete_button_off_texture)
+#	else:
+#		texture = load(close_button_texture)
 
 func _gui_input(event):
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT:
 		var main = get_tree().get_root().find_node("Main", true, false)
 		main._load_overworld(true, is_level_complete)
 
+
+
+func _on_puzzle_completed():
+	texture = load(complete_button_on_texture)
+	is_level_complete = true
